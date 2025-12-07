0342ae4bd692ac5eb8ba0e2bd9ce4618c2c0b590
Ghala <ghalanmasi@icloud.com>
2025-12-07 08:18:56 +0300

    add admin permission

--- SiteLab/custom/admin.py
+++ SiteLab/custom/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import CustomRequest
 
-# Register your models here.
+@admin.register(CustomRequest)
+class CustomRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "project_title",
+        "user",
+        "project_type",
+        "project_focus",
+        "budget_range",
+        "status",
+        "deposit_paid",
+        "final_paid",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "project_type",
+        "project_focus",
+        "deposit_paid",
+        "final_paid",
+        "created_at",
+    )
+
+    search_fields = ("project_title", "user__username", "name", "email")
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+        "deposit_paid_at",
+        "final_paid_at",
+    )
